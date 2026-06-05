be16b6e45b339ed3c638a88143dfbfab4d2f1446
clauds <waguramarie@gmail.com>
2026-06-05 09:41:11 +0200

    adding CNP on ProcessAgent

--- src/main/java/agents/ProcessAgent.java
+++ src/main/java/agents/ProcessAgent.java
@@ -5,6 +5,12 @@ import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPAAgentManagement.FailureException;
 
 public class ProcessAgent extends Agent {
 
@@ -35,6 +41,37 @@ public class ProcessAgent extends Agent {
         } catch (FIPAException fe) {
             fe.printStackTrace();
         }
+        // --- THE CONTRACT NET RESPONDER ---
+        // Create a template so the agent only listens for "Call For Proposal" (CFP) messages
+        MessageTemplate template = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+
+        addBehaviour(new ContractNetResponder(this, template) {
+            @Override
+            protected ACLMessage handleCfp(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
+                System.out.println(getLocalName() + ": Received CFP for a job from " + cfp.getSender().getLocalName());
+
+                // We are available, so we formulate a proposal to do the job
+                ACLMessage propose = cfp.createReply();
+                propose.setPerformative(ACLMessage.PROPOSE);
+                propose.setContent("10"); // We propose an arbitrary "cost" or "time" of 10
+                return propose;
+            }
+
+            @Override
+            protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
+                System.out.println(getLocalName() + ": Proposal accepted! Starting work for " + accept.getSender().getLocalName());
+
+                // TODO later: This is where Process1/Process2 will send the Modbus TCP signal
+                // to the simulation to physically run the machine!
+
+                // For now, we simulate the work instantly finishing and inform the Part
+                ACLMessage inform = accept.createReply();
+                inform.setPerformative(ACLMessage.INFORM);
+                inform.setContent("Process completed");
+                System.out.println(getLocalName() + ": Work finished. Notifying " + accept.getSender().getLocalName());
+                return inform;
+            }
+        });
     }
 
     @Override
